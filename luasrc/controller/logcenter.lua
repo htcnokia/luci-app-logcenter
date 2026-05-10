df2f9573aeb95fc8636b8cc0e30c69376fe7681d
htcnokia <95518596+htcnokia@users.noreply.github.com>
2026-05-10 09:57:07 +0800

    Delete luasrc directory

--- luasrc/controller/logcenter.lua
+++
@@ -1,5 +0,0 @@
-module("luci.controller.logcenter", package.seeall)
-
-function index()
-    entry({"admin", "services", "logcenter"}, cbi("logcenter"), _("LogCenter Timeline"), 30).dependent=false
-end
--- luasrc/model/cbi/logcenter.lua
+++
@@ -1,5 +0,0 @@
-local m = Map("logcenter", "LogCenter Timeline", "View PPPoE, DHCP, SSH, Firewall logs in timeline")
-local s = m:section(TypedSection, "logcenter", "Logs")
-s.template = "cbi/map"
-s.anonymous = true
-return m
